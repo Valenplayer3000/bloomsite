3a66a3731d7fa03835dd15bfda44e1f75d0f16bb
StationaryStation <raulytstation@outlook.com>
2022-05-17 09:49:14 -0400

    feat: Added another drawing

--- src/components/drawing_showcase/index.tsx
+++ src/components/drawing_showcase/index.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 
 const DrawingShowcase = () => {
-    const titles = ["Station Sketch #1"];
-    const descriptions = ["One of the many sketches of his oc"];
+    const titles = ["Station Sketch #1", "No thoughts, Head empty"];
+    const descriptions = ["One of the many sketches of his oc", "Dumb drawing made for the memes and to practice coloring with a base20 colorscheme"];
     
     return (
         <>
@@ -14,6 +14,16 @@ const DrawingShowcase = () => {
             height={256}
             src="/assets/img/station.png"
             alt={titles[0] + " " + descriptions[0]}
+            />
+        </div>
+        <div className="Drawing">
+            <p className="title title-small">{titles[1]}</p>
+            <p className="description">{descriptions[1]}</p>
+            <img
+                width={256}
+                height={256}
+                src="/assets/img/no_thoughts_head_empty.png"
+                alt={titles[1] + " " + descriptions[1]}
             />
         </div>
         </>
