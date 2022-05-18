605f8433a5ca4374baa049ba37f915b349eebf72
Raúl (Station) Pérez <raulytstation@Outlook.com>
2022-05-18 13:31:13 -0400

    feat: Test Footer
    Added Test Footer Component, Testing will be performed at 3:00PM

---
+++ src/components/footer/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <Footer>
+            <a href="stationarystation.checklyhq.app">StationSite Status</a>
+            <p>StationSite / Last Updated: 1:29PM (5/18/2022)</p>
+        </Footer>
+    );
+}
+
+export default Footer;
