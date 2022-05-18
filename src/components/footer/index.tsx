85a0440bb6e4df7b35905819b3e4776390fb8ec3
StationaryStation <raulytstation@outlook.com>
2022-05-18 15:43:10 -0400

    fix: Fix build error
    Fix build error by changing Footer to footer since it is a html element and not a component

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -2,10 +2,10 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <Footer>
+        <footer>
             <a href="stationarystation.checklyhq.app">StationSite Status</a>
             <p>StationSite / Last Updated: 1:29PM (5/18/2022)</p>
-        </Footer>
+        </footer>
     );
 }
 
