06c5fc66839d6f2cc24f92fe8fb807ffecc09cf8
StationaryStation <raulytstation@outlook.com>
2022-05-25 11:29:50 -0400

    feat: use hashrouter (test)

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
 import Navbar from "./components/Navbar"
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
+import { HashRouter, Routes, Route, Navigate } from "react-router-dom"
 import Home from "./pages/Home"
 import About from "./pages/About"
 import NotFound from "./pages/not_found"
@@ -11,18 +11,17 @@ import Stbot from "./pages/StationBot"
 import Footer from "./components/footer"
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <BrowserRouter>
+<HashRouter>
     <React.StrictMode>
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
-        <Route path="/404" element={<NotFound />} />
         <Route path="/projects" element={<ProjectDirectory />} />
         <Route path="/projects/stbot" element={<Stbot />} />
-        <Route path="*" element={<Navigate replace to="/404" />} />
+        <Route path="*" element={<Navigate replace to="/" />} />
       </Routes>
       <Footer />
     </React.StrictMode>
-  </BrowserRouter>
+  </HashRouter>
 )
