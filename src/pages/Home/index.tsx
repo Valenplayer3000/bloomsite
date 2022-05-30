b911f20138d521e691f56ad2d6ab40f9eaf2b12d
Raúl (Station) Pérez <raulytstation@Outlook.com>
2022-05-30 13:55:32 +0000

    post: Add new Post

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -26,6 +26,23 @@ const Home = () => {
         <hr />
         <h1 className='title'>Last Posts</h1>
         <PostsComponent
+          title="Fedora-Powered development"
+          author="Stationarystation"
+          date="09:32 @ May 30, 2022"
+          content="If you didn't knew already, I used arch btw.\n
+            Arch has been my daily driver since the last 5 weeks and It has been going smoothly
+            but there is one small problem with it. It isn't the lightweight part, nor the rolling release part
+            (I am totally fine with that). It's that I have a growing fear of breaking my system so hard
+            that I can't recover any data from it.\n
+            And I don't want to have that at the back of my spine, so I've switched to a much more stabler
+            distribution of linux that you might have heard of, Fedora Linux (Version 36 with Gnome 42.1).\n
+            And for some this might be a downgrade from the absolute charm that Arch brings when
+            you have the right to customize everything from your likings. And I find that amazing for experienced
+            users of GNU/Linux, but I want to focus on what's important, and that is coding.\n
+            
+            Also, Fedora looks clean Out-Of-The-Box, no customization needed (at least for me)."
+        />
+        <PostsComponent
           title='Privacy concerns with social media'
           author='StationaryStation'
           date='09:13 @ May 23, 2022'
