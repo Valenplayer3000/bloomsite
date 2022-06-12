f02180539bd012b973d2b7ea90ef0ae43dd4f93d
Stationarystation <raulytstation@outlook.com>
2022-06-11 21:38:26 -0400

    feat: Refresh UI Colors and add icons

--- components/Navbar/index.tsx
+++ components/Navbar/index.tsx
@@ -5,13 +5,13 @@ import {Info, Home, Box} from "react-feather"
 const Navbar = () => {
   return (
     <div className='navbar'>
-      <Link passHref href='/'>Home</Link>
-      <Link passHref href='/about'><div className="icon"><Info size={24} className='navbar-icon'/><span className="navtooltip">Info</span></div></Link>
+      <Link passHref href='/'><div className="navcontent"><Home size={24} /><p>Home</p></div></Link>
+      <Link passHref href='/about'><div className="navcontent"><Info size={24}/><p>Info</p></div></Link>
       <div className='dropdown'>
-        <Link passHref href='/projects'>Projects</Link>
+        <Link passHref href='/projects'><div className="navcontent"><Box size={24} /><p>Proyects</p></div></Link>
         <div className='dropdown-content'>
-          <Link passHref href='/projects/stbot'>StationBot</Link>
-          <Link passHref href='/projects/mainpage'>Mainpage</Link>
+          <Link passHref href='/projects/stbot'><div>StationBot</div></Link>
+          <Link passHref href='/projects/mainpage'><div>Mainpage</div></Link>
         </div>
       </div>
     </div>
